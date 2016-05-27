8fbcf67b21ab03f6b6905fc4bcaa4982c9595ae0
Mantas Pucka <mantas@8devices.com>
2016-05-27 17:37:44 +0300

    ar71xx: fix issues after merge

--- target/linux/ar71xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -273,7 +273,6 @@ platform_check_image() {
 	unifi | \
 	unifiac | \
 	unifi-outdoor | \
-
 	weio )
 		[ "$magic" != "2705" ] && {
 			echo "Invalid image type."
@@ -326,13 +325,16 @@ platform_check_image() {
 	om2p-hsv2 | \
 	om2p-lc | \
 	om5p | \
-	om5p-an)
+	om5p-an | \
+	om5p-ac | \
+	om5p-acv2)
 		platform_check_image_openmesh "$magic_long" "$1" && return 0
 		return 1
 		;;
 
 	antminer-s1 | \
 	antminer-s3 | \
+	antrouter-r1 | \
 	archer-c5 | \
 	archer-c7 | \
 	el-m150 | \
@@ -340,9 +342,12 @@ platform_check_image() {
 	gl-inet | \
 	mc-mac1200r | \
 	minibox-v1 |\
+	omy-g1 |\
+	omy-x1 |\
 	onion-omega | \
 	oolite | \
 	smart-300 | \
+	tellstick-znet-lite | \
 	tl-mr10u | \
 	tl-mr11u | \
 	tl-mr12u | \
@@ -364,6 +369,7 @@ platform_check_image() {
 	tl-wa901nd | \
 	tl-wa901nd-v2 | \
 	tl-wa901nd-v3 | \
+	tl-wa901nd-v4 | \
 	tl-wdr3320-v2 | \
 	tl-wdr3500 | \
 	tl-wdr4300 | \
@@ -374,6 +380,7 @@ platform_check_image() {
 	tl-wr720n-v3 | \
 	tl-wr741nd | \
 	tl-wr741nd-v4 | \
+	tl-wr810n | \
 	tl-wr841n-v1 | \
 	tl-wa830re-v2 | \
 	tl-wr841n-v7 | \
@@ -401,13 +408,17 @@ platform_check_image() {
 		}
 
 		local hwid
-		local imageid
+		local mid
+		local imagehwid
+		local imagemid
 
 		hwid=$(tplink_get_hwid)
-		imageid=$(tplink_get_image_hwid "$1")
+		mid=$(tplink_get_mid)
+		imagehwid=$(tplink_get_image_hwid "$1")
+		imagemid=$(tplink_get_image_mid "$1")
 
-		[ "$hwid" != "$imageid" ] && {
-			echo "Invalid image, hardware ID mismatch, hw:$hwid image:$imageid."
+		[ "$hwid" != "$imagehwid" -o "$mid" != "$imagemid" ] && {
+			echo "Invalid image, hardware ID mismatch, hw:$hwid $mid image:$imagehwid $imagemid."
 			return 1
 		}
 
@@ -439,7 +450,8 @@ platform_check_image() {
 	wndr3700 | \
 	wnr2000-v3 | \
 	wnr612-v2 | \
-	wnr1000-v2)
+	wnr1000-v2 | \
+	wpn824n)
 		local hw_magic
 
 		hw_magic="$(ar71xx_get_mtd_part_magic firmware)"
@@ -449,8 +461,13 @@ platform_check_image() {
 		}
 		return 0
 		;;
+	mr18)
+		merakinand_do_platform_check $board $1
+		return $?;
+		;;
 	nbg6716 | \
 	r6100 | \
+	rambutan | \
 	wndr3700v4 | \
 	wndr4300 )
 		nand_do_platform_check $board $1
@@ -485,13 +502,20 @@ platform_check_image() {
 		fi
 		return 0
 		;;
-    wnr2000-v4)
+	wnr2000-v4)
 		[ "$magic_long" != "32303034" ] && {
 			echo "Invalid image type."
 			return 1
 		}
 		return 0
 		;;
+	wnr2200)
+                [ "$magic_long" != "32323030" ] && {
+                        echo "Invalid image type."
+                        return 1
+                }
+                return 0
+                ;;
 
 	esac
 
@@ -505,9 +529,13 @@ platform_pre_upgrade() {
 	case "$board" in
 	nbg6716 | \
 	r6100 | \
+	rambutan | \
 	wndr3700v4 | \
 	wndr4300 )
 		nand_do_upgrade "$1"
+		;;
+	mr18)
+		merakinand_do_upgrade "$1"
 		;;
 	esac
 }
@@ -557,7 +585,9 @@ platform_do_upgrade() {
 	om2p-hsv2 | \
 	om2p-lc | \
 	om5p | \
-	om5p-an)
+	om5p-an | \
+	om5p-ac | \
+	om5p-acv2)
 		platform_do_upgrade_openmesh "$ARGV"
 		;;
 	unifi-outdoor-plus | \
