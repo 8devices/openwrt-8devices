2cc3a08bd5b070d8d41f093118342125616d7829
Mantas Pucka <mantas@8devices.com>
2016-06-07 10:30:31 +0300

    ar71xx: lima: stop using memory mapped SPI flash access during startup
    Always use MTD layer so that large flash suport works correctly.
    Also added spidev plat devices for every free HW chip-select.

--- target/linux/ar71xx/files/arch/mips/ath79/mach-lima.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-lima.c
@@ -18,6 +18,7 @@
 #include "dev-gpio-buttons.h"
 #include "dev-leds-gpio.h"
 #include "dev-m25p80.h"
+#include "dev-spi.h"
 #include "dev-usb.h"
 #include "dev-wmac.h"
 #include "machtypes.h"
@@ -27,34 +28,75 @@
 #define LIMA_KEYS_POLL_INTERVAL	20	/* msecs */
 #define LIMA_KEYS_DEBOUNCE_INTERVAL	(3 * LIMA_KEYS_POLL_INTERVAL)
 
-#define LIMA_WMAC_ART_OFFSET	0x1000
-#define LIMA_ETH0_MAC_OFFSET	0x0
-#define LIMA_ETH1_MAC_OFFSET	0x6
 #define LIMA_ETH_PHYS	(BIT(0) | BIT(1))
 
 static struct gpio_keys_button lima_gpio_keys[] __initdata = {
 	{
-		.desc		= "wps button",
+		.desc		= "button0",
 		.type		= EV_KEY,
-		.code		= KEY_WPS_BUTTON, /* TODO: check code */ 
+		.code		= BTN_0,
 		.debounce_interval = LIMA_KEYS_DEBOUNCE_INTERVAL,
 		.gpio		= LIMA_GPIO_BTN_RST_DEFAULT,
 		.active_low	= 1,
 	}
 };
 
+static struct ath79_spi_controller_data lima_spi0_cdata =
+{
+	.cs_type = ATH79_SPI_CS_TYPE_INTERNAL,
+	.cs_line = 0,
+};
+
+static struct ath79_spi_controller_data lima_spi1_cdata =
+{
+	.cs_type = ATH79_SPI_CS_TYPE_INTERNAL,
+	.cs_line = 1,
+};
+
+static struct ath79_spi_controller_data lima_spi2_cdata =
+{
+	.cs_type = ATH79_SPI_CS_TYPE_INTERNAL,
+	.cs_line = 2,
+};
+
+static struct spi_board_info lima_spi_info[] = {
+	{
+		.bus_num	= 0,
+		.chip_select	= 0,
+		.max_speed_hz	= 25000000,
+		.modalias	= "m25p80",
+		.controller_data = &lima_spi0_cdata,
+	},
+	{
+		.bus_num	= 0,
+		.chip_select	= 1,
+		.max_speed_hz   = 25000000,
+		.modalias	= "spidev",
+		.controller_data = &lima_spi1_cdata,
+	},
+	{
+		.bus_num	= 0,
+		.chip_select	= 2,
+		.max_speed_hz   = 25000000,
+		.modalias	= "spidev",
+		.controller_data = &lima_spi2_cdata,
+	}
+};
+
+static struct ath79_spi_platform_data lima_spi_data =
+{
+	.bus_num		= 0,
+	.num_chipselect 	= 3,
+	.use_hw_shiftreg 	= true,
+};
+
 static void __init lima_setup(void)
 {
-	u8 *art = (u8 *)KSEG1ADDR(0x1f080000);
+	ath79_register_spi(&lima_spi_data, lima_spi_info, 3);
 
-	ath79_register_m25p80(NULL);
 	ath79_register_gpio_keys_polled(-1, LIMA_KEYS_POLL_INTERVAL,
 					ARRAY_SIZE(lima_gpio_keys),
 					lima_gpio_keys);
-
-	ath79_register_usb();
-	ath79_register_pci();
-	ath79_register_wmac(art + LIMA_WMAC_ART_OFFSET, NULL);
 
 	ath79_setup_ar933x_phy4_switch(true, true);
 	ath79_register_mdio(0, ~LIMA_ETH_PHYS);
@@ -65,16 +107,18 @@ static void __init lima_setup(void)
 	ath79_eth1_data.phy_if_mode = PHY_INTERFACE_MODE_GMII;
 	ath79_eth1_data.duplex = DUPLEX_FULL;
 	ath79_eth1_data.phy_mask = BIT(1);
-	ath79_init_mac(ath79_eth1_data.mac_addr, art + LIMA_ETH0_MAC_OFFSET, 0);
 	ath79_register_eth(1);
-	
+
 	/* WAN */
 	ath79_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
 	ath79_eth0_data.duplex = DUPLEX_FULL;
 	ath79_eth0_data.speed = SPEED_100;
 	ath79_eth0_data.phy_mask = BIT(0);
-	ath79_init_mac(ath79_eth0_data.mac_addr, art + LIMA_ETH1_MAC_OFFSET, 0);
 	ath79_register_eth(0);
+
+	ath79_register_wmac_simple();
+	ath79_register_usb();
+	ath79_register_pci();
 }
 
 MIPS_MACHINE(ATH79_MACH_LIMA, "LIMA", "8devices Lima board", lima_setup);
