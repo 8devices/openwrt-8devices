3b4e0d8b419e02688a440bc9783b944f8266d762
Mantas Pucka <mantas@8devices.com>
2016-06-23 13:05:08 +0300

    build: support multiple configs in build script

--- build.sh
+++ build.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
+help(){
+    echo "Usage: $0 [board-name]"
+    echo "valid board names: carambola2 centipede lima rambutan"
+    exit
+}
+
+if [[ $# -lt 1 ]]
+then
+    config=config_minimal
+else
+    case $1 in
+        rambutan)
+            config=config_rambutan_minimal
+            ;;
+        carambola2|centipede|lima)
+            config=config_minimal
+            ;;
+        help|--help|-h|*)
+            help
+            ;;
+    esac
+fi
+
+echo "Building image using config: '$config'"
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 cp .config .config_save`date +%F_%T.%N`
-cp config_minimal .config
+cp $config .config
 make defconfig
 make -j4 IGNORE_ERRORS=m
