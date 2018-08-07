25628e289b5097cd66ffb0726d77918fa7646483
Mantas Pucka <mantas@8devices.com>
2018-08-07 16:26:25 +0300

    kinkan: add build script

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+help(){
+    echo "Usage: $0 [board-name]"
+    echo "valid board names: kinkan"
+    exit
+}
+
+if [[ $# -lt 1 ]]
+then
+    config=config_kinkan_minimal
+else
+    case $1 in
+        kinkan)
+            config=config_kinkan_minimal
+            ;;
+        help|--help|-h|*)
+            help
+            ;;
+    esac
+fi
+
+echo "Building image using config: '$config'"
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+cp .config .config_save`date +%F_%T.%N`
+cp $config .config
+make defconfig
+make -j4 IGNORE_ERRORS='m n y'
