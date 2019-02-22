f6b01947e003716313105254928843b0163f49fd
Mantas Pucka <mantas@8devices.com>
2019-02-22 10:46:37 +0200

    kernel: reorganize file for easier auto-merge

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -2,13 +2,18 @@
 
 LINUX_RELEASE?=1
 
-LINUX_VERSION-3.18 = .122
 LINUX_VERSION-4.9 = .120
 LINUX_VERSION-4.14 = .63
 
-LINUX_KERNEL_HASH-3.18.122 = 675b1ce36af23caa500cb1d4f0ec2976791fb0a97ebb6486a5e2ebcb5527ade5
 LINUX_KERNEL_HASH-4.9.120 = d75af506865edc8145a344c4e73c3bb1000e6c9f1c3489b8dae47ca8f033fd91
 LINUX_KERNEL_HASH-4.14.63 = cd2e52f0e7ba861afa91cf487b2f45e5174115870f256a1d65996647b7bcc6d3
+
+
+# Legacy kernel
+
+LINUX_VERSION-3.18 = .122
+LINUX_KERNEL_HASH-3.18.122 = 675b1ce36af23caa500cb1d4f0ec2976791fb0a97ebb6486a5e2ebcb5527ade5
+
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
