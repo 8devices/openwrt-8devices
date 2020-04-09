da1054a330b0685510252d508b0d087a6345d4d4
Mantas Pucka <mantas@8devices.com>
2020-04-10 00:53:20 +0300

    build: add configs and build script

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CONFIGS=$(ls | awk '/config_/{sub(/config_/,""); print}')
+
+help(){
+    echo "Usage: $0 [board-name]"
+    echo "valid config names:"
+    echo  "$CONFIGS"
+    exit
+}
+
+if [[ $# -lt 1 ]]
+then
+    config=config_8dev_ipq40xx_minimal
+else
+    echo "$CONFIGS" | grep "^$1\$" > /dev/null && config=config_$1 ||\
+    help
+fi
+
+echo "Building image using config: '$config'"
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+cp .config .config_save`date +%F_%T.%N`
+cp $config .config
+make defconfig
+make -j4 IGNORE_ERRORS='m n y'
