e6a4498f685c14c7d9269ac7e36c25bdab32d6ab
Tomas C <tomas.c@8devices.com>
2021-02-23 11:25:55 +0000

    Fix compile errors

--- qca/src/linux-4.4/include/linux/mm.h
+++ qca/src/linux-4.4/include/linux/mm.h
@@ -231,7 +231,7 @@ struct vm_fault {
 	unsigned int flags;		/* FAULT_FLAG_xxx flags */
 	pgoff_t pgoff;			/* Logical page offset based on vma */
 	void __user *virtual_address;	/* Faulting virtual address */
-
+	unsigned long address;		/* Faulting virtual address */
 	struct page *cow_page;		/* Handler may choose to COW */
 	struct page *page;		/* ->fault handlers should return a
 					 * page here, unless VM_FAULT_NOPAGE
